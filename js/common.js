22ee5d36c3af2c723c83b97483b6e52fe233ad73
nicocarnebia <nicocarnebia@gmail.com>
2015-10-25 18:00:50 -0200

    added shadow loader

--- js/common.js
+++ js/common.js
@@ -8,8 +8,6 @@
  * Url principal
  */
 var BASE_URL = 'http://www.localhost.com:8079/PhonegapPFinal/Multideportivo-Web-Gap/';
-
-
 /**
  * 
  * URLS DE WEBSERVICES
@@ -22,8 +20,6 @@ var WS_URLS =
             OTRO_WS: "http://www......com/",
             ETC: "http://www.......com/"
         };
-
-
 /**
  * 
  * Devuelve el panel (menu a la izquierda) y lo inserta en el componente con el id panelId.
@@ -36,4 +32,30 @@ function loadMainPanel(panelId) {
     console.log("Main panel created and appended to " + panelId + ".");
     return r;
 }
- 
+/**
+ * Prende o apaga el loader screen
+ * @param {bollean} toggle expects true or false
+ * @returns {void}
+ */
+function loader(toggle) {
+    var shadow = $("#absolute-shadow");
+    if (shadow.length < 1) {
+        $("body").prepend("<div id='absolute-shadow'></div>");
+        shadow = $("#absolute-shadow");
+    }
+    //add style
+    shadow.css({
+        'background': 'black',
+        'opacity': '0.8',
+        'left': '0',
+        'top': '0',
+        'width': '100%',
+        'height': '100%',
+        'position': 'absolute'
+    });
+    if (toggle) {
+        shadow.fadeIn();
+    } else {
+        shadow.fadeOut();
+    }
+}
--- pages/evento/js/script.js
+++ pages/evento/js/script.js
@@ -1,3 +1,4 @@
+/* pagination vars */
 var current_page = 1;
 var total_per_page = 10;
 
@@ -5,30 +6,59 @@ $(document).ready(function () {
     console.log("Eventos document ready.");
     loadMainPanel("eventMainPanel");//loads the main panel
     listEvents();//se listan los eventos al cargar la pagina
+
+    //Jquery Events
+    //click on delete event button
+    $("#listEventsContent").on("click", ".mainEventDelete", function (e) {
+        e.preventDefault();
+        var evntid = $(this).attr("data-id");
+        //TODO: redirect to erase event Url
+    });
+    //click on event  detail button
+    $("#listEventsContent").on("click", ".mainEventDetail", function (e) {
+        e.preventDefault();
+        var evntid = $(this).attr("data-id");
+        //TODO: redirect to event detail Url
+    });
+    //click on event  edit button
+    $("#listEventsContent").on("click", ".mainEventEdit", function (e) {
+        e.preventDefault();
+        var evntid = $(this).attr("data-id");
+        //TODO: redirect to event edit Url
+    });
 });
 
 
 function listEvents() {
     var limit = (current_page - 1) * total_per_page;
     var offset = current_page * total_per_page;
+    loader(true);
     $.ajax({
         type: "GET",
         dataType: "json",
         url: WS_URLS.EVENTOS_LISTAR_DESDE_HASTA + limit + "/" + offset,
         success: function (data) {
-            console.log(data);
+            if (data.length === 0)
+                displayEmptyListMessage();
             $.each(data, function (i, evnt) {
                 console.log(evnt);
-                var html = parseEventToHtml(evnt);
+                var html = parseMainEventToHtml(evnt);
                 $("#listEventsContent").append(html);
             });
+            loader(false);
         },
     });
 }
-function parseEventToHtml(event) {
-    return '<li><h4>' +
+function displayEmptyListMessage() {
+    $("#listEventsContent").append('<div class="notice">No hay nada por aquí.</div>');
+}
+function parseMainEventToHtml(event) {
+    return '<li class="mainEvent" data-id="' + event.mainCompetitionId + '"><h4>' +
             event.startDate + " " + event.endDate +
-            '</h4><h4>' + event.name +
-            '</h4><p>' + event.description +
-            '</p></li>';
+            '</h4><h4>' + event.name + '</h4><p>' + event.description +
+            '</p>' +
+            '<a class="mainEventDelete"  data-id="' + event.mainCompetitionId + '" href="#">Eliminar</a>' +
+            '<a class="mainEventDetail"  data-id="' + event.mainCompetitionId + '" href="#">Ver</a>' +
+            '<a class="mainEventEdit"  data-id="' + event.mainCompetitionId + '" href="#">Editar</a>' +
+            '</li>';
 }

bb6b3cf5850016ece9d457a8bf5a78d0009a7f30
nicocarnebia <nicocarnebia@gmail.com>
2015-10-26 00:51:33 -0200

    changes

--- pages/login/js/script.js
+++ pages/login/js/script.js
@@ -1,38 +1,41 @@
 // JavaScript Document
-  $("#log_in").on("click", function (e) {
-console.log("ok");
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-console.log(username);
-//loader(true);
-$.ajax({
+$("#log_in").on("click", function (e) {
+    e.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    login(username, password);
 
- type: "POST",
+});
 
- url: "http://tecnocompetition.ddns.net:8080/pfinal/services/adminauth", 
-
-data: JSON.stringify({ "username": username,"pasword":password }), 
-
-dataType: "json",
-success: function(data){ 
-console.log(data);
-var token=data.token;//extraer el token del mensaje
-localStorage.setItem("token", token); //guarda en la cache el token
+function login(username, password) {
+    var user = {"username": username, "pasword": password};
+    console.log(JSON.stringify(user));
+    $.ajax({
+        type: "POST",
+        url: "http://tecnocompetition.ddns.net:8080/pfinal/services/adminauth",
+        data: JSON.stringify(user),
+        dataType: "json", crossDomain: true,
+        accept: {
+            json: 'application/json',
+            xml: 'application/xml'
+        },
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            console.log(data);
+            var token = data.token;//extraer el token del mensaje
+            localStorage.setItem("token", token); //guarda en la cache el token
 //loader(false);
-console.log("ok");
-alert('Logueado correctamente.'); 
-}, 
-
-statusCode: { 
-401: 
-function() { 
-alert(CONSTANTE_ERROR_MESSAGE_NO_AUTORIZADO); 
-}, 
-500: function() {
- alert(CONSTANTE_ERROR_MESSAGE_SERVIDOR_500); 
-} 
-} 
-});
-});
-var username = localStorage.getItem('username');
-console.log(username);
+            console.log("success");
+            alert('Logueado correctamente.');
+        },
+        statusCode: {
+            401:
+                    function () {
+                        alert(CONSTANTE_ERROR_MESSAGE_NO_AUTORIZADO);
+                    },
+            500: function () {
+                alert(CONSTANTE_ERROR_MESSAGE_SERVIDOR_500);
+            }
+        }
+    });
+}

5074de0cc876faf9626c0a9d57627364029e9757
Bleiben1 <rodri47hn@gmail.com>
2015-10-27 10:59:38 -0300

    se trabajó en agregar un evento, requiere comprobacion de funcionamiento

--- js/script.js
+++ js/script.js
@@ -20,13 +20,12 @@ $(document).ready(function () {
 
     //--------------prueba-------------
     $("body").on("click", "#idEventos", function (e) {
-		cosole.log("getting events form ws, please wait...");
+		console.log("getting events form ws, please wait...");
         $("#infoContainer").load("pages/evento/evento.html");
     });
     //--------------/prueba------------
-				$("#log-in").click(function(event) {
-					$("#content").load('pages/login/login.html');
-				});
-
+	$("#log-in").click(function(event) {
+		$("#content").load('pages/login/login.html');
+	});
 
 });

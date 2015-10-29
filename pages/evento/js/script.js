58389f137a4cfbfa245313b3566600f3b5ad9467
Bleiben1 <rodri47hn@gmail.com>
2015-10-29 16:04:59 -0300

    se trabajo en modificar un evento, cada vez mas cerca

--- pages/evento/js/script.js
+++ pages/evento/js/script.js
@@ -20,24 +20,37 @@ $(document).ready(function () {
     });
 	
 	$("body").on("click", ".btnModif", function (e) {
-//	$(".btnModif").click(function(){
         console.log("Sending modified event data to ws");
-		//alert($(this).data("data-event-Name"));
-//data-event-Name="' + event.name + '" data-event-description="' + event.description + '" data-event-startDate="' + event.startDate + '" data-event-endDate=
-        var newEventStartDate = $(this).data("data-event-startdate");
-        var newEventEndDate = $(this).data("data-event-enddate");
-        //var newEventName = $(this).data("data-event-name");
+        var eventId = $(this).attr("id");
+        var newEventStartDate = "" + $(this).attr("data-event-startdate");
+        var newEventEndDate = "" + $(this).attr("data-event-enddate");
 		var newEventName = $(this).attr("data-event-name");
-        var newEventDesc = $(this).data("data-event-description");
-        /*var newEventStartDate = document.getElementById("newEventStartDate").value;
-        var newEventStartTime = document.getElementById("newEventStartTime").value;
-        var newEventEndDate = document.getElementById("newEventEndDate").value;
-        var newEventEndTime = document.getElementById("newEventEndTime").value;*/
+        var newEventDesc = $(this).attr("data-event-description");
+		$("#eventId").attr("value", function(){return eventId});
 		$("#newEventName2").attr("value", function(){return newEventName;});
-		/*var newEventDesc = document.getElementById("newEventDesc").value;
+		$("#newEventDesc2").val(newEventDesc);
+		var startDate = newEventStartDate.substring(0,10);
+		$("#newEventStartDate2").attr("value", function(){return startDate;});
+		var startHour = newEventStartDate.substring(11,16);
+		$("#newEventStartTime2").attr("value", function(){return startHour;});
+		var endDate = newEventEndDate.substring(0,10);
+		$("#newEventEndDate2").attr("value", function(){return endDate;});
+		var endHour = newEventEndDate.substring(11,16);
+		$("#newEventEndTime2").attr("value", function(){return endHour;});
+    });
+	
+    $("body").on("click", "#btnModifEvent", function (e) {
+        console.log("Adding new event to ws");
+		var eventId  = document.getElementById("eventId").value;
+        var newEventStartDate = document.getElementById("newEventStartDate2").value;
+        var newEventStartTime = document.getElementById("newEventStartTime2").value;
+        var newEventEndDate = document.getElementById("newEventEndDate2").value;
+        var newEventEndTime = document.getElementById("newEventEndTime2").value;
+        var newEventName = document.getElementById("newEventName2").value;
+        var newEventDesc = document.getElementById("newEventDesc2").value;
         var newEventStart = newEventStartDate + "T" + newEventStartTime+ ":00";
         var newEventEnd = newEventEndDate + "T" + newEventEndTime+ ":00";
-        addEvent(newEventStart, newEventEnd, newEventName, newEventDesc);*/
+        modifEvent(eventId, newEventStart, newEventEnd, newEventName, newEventDesc);
     });
 });
 
@@ -64,21 +77,22 @@ function parseEventToHtml(event) {
     return '<li class="list-group-item"><div class="thumbnail"><div class="caption"><h3>' +
             event.startDate + " " + event.endDate +
             '</h3><h2>' + event.name + '</h2><p>' + event.description +
-            '</p><a class="btn btn-primary btnModif" href="#" data-toggle="modal" data-target="#modifEventModal" data-event-name="' + event.name + '" data-event-description="' + event.description + '" data-event-startdate="' + event.startDate + '" data-event-enddate="' + event.endDate + '">Modificar</a> <a class="btn btn-primary" href="#">Borrar</a></div></div></li>';
+            '</p><a id="' + event.mainCompetitionId + '" class="btn btn-primary btnModif" href="#" data-toggle="modal" data-target="#modifEventModal" data-event-name="' + event.name + '" data-event-description="' + event.description + '" data-event-startdate="' + event.startDate + '" data-event-enddate="' + event.endDate + '">Modificar</a> <a class="btn btn-primary" href="#">Borrar</a></div></div></li>';
     '</p></div></div></li>';
 }
 
-function addEvent(newEventStart, newEventEnd, newEventName, newEventDesc) {
+function modifEvent(eventId, newEventStart, newEventEnd, newEventName, newEventDesc) {
     var arr = {description: newEventDesc, endDate: newEventEnd, name: newEventName,startDate: newEventStart, styleType: 0 };
     console.log(arr);
     var token = localStorage.getItem('token');
     console.log(token);
+	var theURL = "http://tecnocompetition.ddns.net:8080/pfinal/services/entities.maincompetition/" + eventId;
     $.ajax({
-        url: 'http://tecnocompetition.ddns.net:8080/pfinal/services/entities.maincompetition/',
-        type: 'POST',
-        /*headers: {
-            "token": token
-        },*/
+        url: theURL,
+        type: 'PUT',
+        headers: {
+			"Authorization":"oauth" + token
+        },
         data: JSON.stringify(arr),
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
@@ -90,7 +104,7 @@ function addEvent(newEventStart, newEventEnd, newEventName, newEventDesc) {
     });
 }
 
-function modifyEvent(newEventStart, newEventEnd, newEventName, newEventDesc) {
+function addEvent(newEventStart, newEventEnd, newEventName, newEventDesc) {
     var arr = {description: newEventDesc, endDate: newEventEnd, name: newEventName,startDate: newEventStart, styleType: 0 };
     console.log(arr);
     var token = localStorage.getItem('token');
